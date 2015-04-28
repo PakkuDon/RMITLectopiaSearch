2b3b2ee704b3a856cf59621a022db45079b1958e
PakkuDon <pakku.pd@gmail.com>
2015-04-29 07:51:57 +1000

    Refactoring - Moved paging functionality to another module
    - Added a Pager class which handles the process of creating pages for
      content.
    - Edited index.html JS code to use Pager class.

---
+++ js/pager.js
@@ -0,0 +1,84 @@
+/**
+ * Helper class - generates HTML to display elements of an array
+ * in a paged format.
+ */
+function Pager() {
+    /**
+     * createPagedList() - Constructs a HTML string containing divs for
+     * each page and a set of links to navigate between pages.
+     * params
+     * elements: An array of items that will make up the page body.
+     * header: Content that is displayed at the top of each page.
+     * footer: Content that is displayed at the end of each page.
+     * pageSize: Number of elements to display in each page.
+     */
+    this.createPagedList = function(elements, header, footer, pageSize) {
+        var pageCount = Math.ceil(elements.length / pageSize);
+        var links = this.createPageLinks(pageCount);
+        var pages = this.createPages(elements, header, footer, pageSize);
+
+        return links + pages;
+    }
+
+    /**
+     * createPages() - Constructs a HTML string containing the given elements
+     * spread over separate 'page' divs.
+     * params
+     * elements: An array of items that will make up the page body.
+     * header: Content that is displayed at the top of each page.
+     * footer: Content that is displayed at the end of each page.
+     * pageSize: Number of elements to display in each page.
+     */
+    this.createPages = function(elements, header, footer, pageSize) {
+        var text = "";
+        var pageCount = Math.ceil(elements.length / pageSize);
+
+        for (var i = 0; i < pageCount; i++) {
+            // Fetch next page's content
+            var pageContent = elements.splice(0, pageSize).join("");
+            // Append new page to result
+            text += "<div class=\"page" +  (i + 1) + "\">"
+                + header
+                + pageContent
+                + footer
+                + "</div>";
+        }
+
+        return text;
+    }
+
+    /**
+     * createPageLinks() - Constructs a HTML string containing links
+     * to navigate between pages.
+     * params
+     * pageCount: Number of pages in collection.
+     */
+    this.createPageLinks = function(pageCount) {
+        var text = "<div>";
+        for (var i = 0; i < pageCount; i++) {
+            var pageNum = i + 1;
+
+            // Wrap each page number in a span element
+            text += "<span class=\"button\" id=\"page"
+                + pageNum
+                + "\">"
+                + pageNum
+                + "</span>";
+        }
+        text += "</div>";
+        return text;
+    }
+
+    /**
+     * showPage() - Callback method. Switches current page to page
+     * denoted by pageId
+     * params
+     * pageId: Name of the selected page.
+     */
+    this.showPage = function(pageId) {
+        // Hide other pages
+        $("div[class^=page]").hide();
+        // Display associated page
+        $("." + pageId).show();
+    }
+}
